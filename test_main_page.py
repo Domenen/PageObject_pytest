250eb92ba99301cff2679f9d2c127859ad861b5d
Domenen <Diman.9533@gmail.com>
2025-03-04 12:27:15 +0300

    'new_login_page_and_test_for_login_page'

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,23 @@
-from selenium.webdriver.common.by import By
-
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
-link = "http://selenium1py.pythonanywhere.com/"
+
+main_link = "http://selenium1py.pythonanywhere.com/"
+login_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
 # pytest -v --tb=line --language=en test_main_page.py
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)
+    page = MainPage(browser, main_link)
     page.open()
     page.go_to_login_page
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, main_link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_should_see_login_form(browser):
+    page = LoginPage(browser, login_link)
+    page.open()
+    page.should_be_login_page()
